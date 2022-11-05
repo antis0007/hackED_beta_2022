bc108cf74fefaa0e73d69f81ad25f177ad5b590f
Logan9312 <lgntravis@gmail.com>
2022-11-05 15:05:11 -0600

    Pushing Changes

---
+++ src/components/player.js
@@ -0,0 +1,21 @@
+class Player {
+    hand = []
+    constructor(deck) {
+        this.hand = deck
+    }
+
+    DrawCard(deck) {
+        //Draws a card from the specified deck
+        deck.DrawCard()
+        this.hand.AddCard(hand)
+    }
+
+    PlayCard() {
+        // 
+    }
+
+    Discard() {
+        // Adds Card to Discard Pile
+    }
+    
+}
